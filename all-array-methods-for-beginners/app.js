c8c481146f823caa2881a0caf8f750e98154c278
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-05-03 09:34:23 +0200

    Add files via upload

--- all-array-methods-for-beginners/app.js
+++ all-array-methods-for-beginners/app.js
@@ -90,3 +90,145 @@ const result3 = mixNums.filter((value) => {
 
 // 4th way
 const result5 = mixNums.filter((value) => value > 5); // WOW!!!
+
+// =================================================================
+// find()
+// Array.find(callBackFn)
+// function name(value, index, array){
+//   return condition
+// }
+
+const users = [
+  { name: "Ali", age: 19 },
+  { name: "Usman", age: 20 },
+  { name: "Umar", age: 21 },
+];
+
+// 1st way
+function ali(user) {
+  return user.name === "Ali";
+}
+
+const findRes = users.find(ali);
+
+// 2nd way
+const findRes1 = users.find(function (user) {
+  return user.name === "Ali";
+});
+
+// 3rd way
+const findRes2 = users.find((user) => {
+  return user.name === "Ali";
+});
+
+// 4th way
+const findRes3 = users.find((user) => user.name === "Ali");
+// console.log(findRes3);
+
+// =================================================================
+// findIndex()
+function ali(user) {
+  return user.name === "Ali";
+}
+
+const index = users.findIndex(ali);
+// console.log(index);
+
+// =================================================================
+// some()
+const fruits = [{ name: "banana" }, { name: "tomato" }, { name: "grapes" }];
+
+let hasTomato = false;
+for (let i = 0; i < fruits.length; i++) {
+  const item = fruits[i];
+  if (item.name === "tomato") {
+    hasTomato = true;
+    break;
+  }
+}
+
+// Array.some(callBackFn)
+// function name(value, index, array) {
+
+// };
+
+// 1st way
+function tomato(item) {
+  return item.name === "tomato";
+}
+
+const hasTomato1 = fruits.some(tomato);
+
+// 2nd way
+const hasTomato2 = fruits.some(function (item) {
+  return item.name === "tomato";
+});
+
+// 3rd way
+const hasTomato3 = fruits.some((item) => {
+  return item.name === "tomato";
+});
+
+// 4th way
+const hasTomato4 = fruits.some((item) => item.name === "tomato");
+
+//================================================================
+// every()
+
+const nums = [5, -2, 4, 1];
+let allPositive = true;
+
+for (let i = 0; i < nums.length; i++) {
+  const n = nums[i];
+  if (n >= 0) {
+    continue;
+  } else {
+    allPositive = false;
+    break;
+  }
+}
+
+// with every()
+// 1st way
+function positive(n) {
+  return n >= 0;
+}
+
+const everyRes = nums.every(positive);
+
+// 2nd way
+const everyRes1 = nums.every(function (n) {
+  return n >= 0;
+});
+
+// 3rd way
+const everyRes2 = nums.every((n) => {
+  return n >= 0;
+});
+
+// 4th way
+const everyRes3 = nums.every((n) => n >= 0);
+
+//================================================================
+// reduce()
+
+const cart = [
+  { name: "Laptop", price: 1100 },
+  { name: "Mouse", price: 400 },
+  { name: "Keyboard", price: 900 },
+];
+
+let total = 0;
+
+for (let i = 0; i < cart.length; i++) {
+  const item = cart[i];
+  total = total + item.price;
+}
+
+// with reduce()
+
+function totalaPrice(accumulator, item) {
+  return accumulator + item.price;
+}
+
+const totalResult = cart.reduce(totalaPrice, 0);
