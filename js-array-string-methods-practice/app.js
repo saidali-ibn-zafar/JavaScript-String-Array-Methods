96a128537f751fd2a6b0df00c266bac0a0342537
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-05-07 10:10:50 +0200

    Add files via upload

--- js-array-string-methods-practice/app.js
+++ js-array-string-methods-practice/app.js
@@ -68,10 +68,134 @@ console.log(sortedArray); // Output: [2, 4, 6, 1, 3, 5]
 
 // const zeroArr = [];
 
-
 const arr_1 = new Array(100).fill(0);
-console.log(arr_1)
+console.log(arr_1);
 
-
-const arr_2 = Array.from({length: 100}, (_, i) => i + 1);
+const arr_2 = Array.from({ length: 100 }, (_, i) => i + 1);
 console.log(arr_2);
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// The Array() constructor creates Array objects.
+
+// new Array()
+// new Array(element0)
+// new Array(element0, element1)
+// new Array(element0, element1, /* … ,*/ elementN)
+// new Array(arrayLength)
+
+// Array()
+// Array(element0)
+// Array(element0, element1)
+// Array(element0, element1, /* … ,*/ elementN)
+// Array(arrayLength)
+
+const filledWithZeroArr = new Array(100).fill(0);
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// at()
+const numsArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// console.log(numsArr.at(0));
+// console.log(numsArr.at(numsArr.length - 1));
+// console.log(numsArr.at(numsArr.length / 2));
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+
+console.log(numsArr.copyWithin(0, numsArr.length / 2, numsArr.length / 2 + 1));
+console.log(numsArr.copyWithin(1, 2, 3));
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// entries()
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const f = fruits.entries();
+
+for (const x of f) {
+  console.log(x);
+}
+
+for (const element of [, "a"].entries()) {
+  console.log(element);
+}
+// [0, undefined]
+// [1, 'a']
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// call()
+const person = {
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+const person1 = {
+  firstName: "Ali",
+  lastName: "Batirov",
+};
+const person2 = {
+  firstName: "Jasur",
+  lastName: "Sabitov",
+};
+
+// This will return "Ali Batirov";
+const fullName = person.fullName.call(person1);
+console.log(fullName);
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// every()
+
+const evenNums = [2, 4, 6, 8];
+const res = evenNums.every((num) => num % 2 === 0);
+
+const isSubset = (arr1, arr2) =>
+  arr2.every((element) => arr1.includes(element));
+
+console.log(isSubset([1, 2, 3, 4], [4, 3, 6]));
+
+// = = = = = = = = = = = = = = = = = = = = = = =
+// filter()
+const arrayLike = {
+  length: 3,
+  0: "a",
+  1: "b",
+  2: "c",
+};
+
+console.log(Array.prototype.filter.call(arrayLike, (x) => x <= "b"));
+
+// Appending new words
+words = ["spray", "limit", "exuberant", "destruction", "elite", "present"];
+const appendedWords = words.filter((word, index, arr) => {
+  arr.push("new");
+  return word.length < 6;
+});
+
+// = = = = = = = = = = = = = = = = = = = = = = = =
+// find()
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find((element) => element === 44);
+
+console.log(array1.indexOf(found));
+// Expected output: 4
+
+function isPrime(element) {
+  if (element % 2 === 0 || element < 2) {
+    return false;
+  }
+  for (let factor = 3; factor <= Math.sqrt(element); factor += 2) {
+    if (element % factor === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log([4, 5].findIndex(isPrime));
+
+// = = =
+const newArr = [12, 22, 32, 42, 52];
+
+const result = Array.prototype.filter.call(newArr, (x) => x <= 3);
+// = = =
+
+const res1 = Array.prototype.findLast.call(newArr, (x) => x > 45);
+
